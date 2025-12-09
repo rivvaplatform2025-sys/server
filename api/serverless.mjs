cd6939ff033858903ad802b98b8dcb45d6366135
Samuel Esezobor <samuel.esezobor076@gmail.com>
2025-12-09 09:18:54 +0100

    commit redirect start:prod to have a dist/src/main.js

--- api/serverless.mjs
+++
@@ -1,2 +0,0 @@
-import handler from '../dist/serverless.js';
-export default handler;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,13 +31,4 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
-  {
-    files: ['api/**/*.mjs'],
-    languageOptions: {},
-    rules: {
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-    },
-  },
 );
